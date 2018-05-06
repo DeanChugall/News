059045b9346c1348f9ce319f9c214ece838d2481
Dean <deanchugall@gmail.com>
2018-05-07 01:18:56 +0200

    Initial

---
+++ app/src/main/java/xyz/cugalj/news/utillity/FixImageExtension.java
@@ -0,0 +1,6 @@
+package xyz.cugalj.news.utillity;
+
+public class FixImageExtension {
+
+    //TODO fix images extension because some of the images thumbnail is no properly named( instead .jpg they give -jpg )
+}
